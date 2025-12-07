1cdb4b3769d01125bb401e40825180b79c7949a4
Tonmoy <tonmoybiswas20602@gmail.com>
2025-12-07 18:16:28 +0600

    authentication complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar bg-base-100 shadow-lg">
+    <div className="navbar bg-base-100  shadow-lg">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
