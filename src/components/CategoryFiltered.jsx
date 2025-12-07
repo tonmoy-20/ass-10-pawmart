7e4d1b84e7fbe53c2b4328c4b6b4eea5075b6a3c
Tonmoy <tonmoybiswas20602@gmail.com>
2025-12-07 19:22:28 +0600

    almost done

--- src/components/CategoryFiltered.jsx
+++ src/components/CategoryFiltered.jsx
@@ -11,7 +11,31 @@ const CategoryFiltered = () => {
   }, [categoryName]);
 
   return (
-   
+    <div className="px-4 md:px-20 py-10">
+      <h2 className="text-3xl font-bold mb-6 capitalize">
+        {categoryName} Products
+      </h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {services.map((p) => (
+          <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+              <img src={p?.image} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title font-bold">
+                {p?.name}
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+
+              <p className="font-lg">{p?.description}</p>
+
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">{p?.category}</div>
+                <div className="badge badge-outline">$ {p?.price}</div>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
